41e2fa6ea9e06bc8c035c466f75538aeca9c4fc0
Olly Larkin <ollylemonlarkin@gmail.com>
2021-06-06 14:17:55 +0100

    Added library functions

--- StdInStdOutComm/src/StdInStdOutCommLibrary/Library.fs
+++ StdInStdOutComm/src/StdInStdOutCommLibrary/Library.fs
@@ -1,5 +1,39 @@
-﻿namespace StdInStdOutCommLibrary
+﻿module StdComm
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+/// Size of the message header in bytes
+[<Literal>]
+let HeaderSize = 4
+
+let receive () =
+    let getInt () =
+        let mutable outVal = -1
+        while outVal = -1 do
+            outVal <- Console.Read()
+        outVal
+
+    let getChar () = char <| getInt()
+    let getString () = string <| getChar()
+
+    let size =
+        [0 .. HeaderSize - 1]
+        |> List.map (fun _ -> getInt())
+        |> List.indexed
+        |> List.map (fun (i, value) -> value <<< (i * 8))
+        |> List.reduce (+)
+    
+    [0 .. size - 1]
+    |> List.map (fun _ -> getString())
+    |> List.reduce (+)
+
+let send (message: string) = 
+    let size = message.Length
+    let header =
+        (size, [0 .. HeaderSize - 1])
+        ||> List.mapFold (fun s _ -> 
+            let str = (s &&& 15) |> char |> string
+            str, (s >>> 8))
+        |> fst
+        |> List.reduce (+)
+    printf "%s" (header + message)
